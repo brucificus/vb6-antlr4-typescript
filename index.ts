c44d46a251339237606e220a3bdb0111adb56e1f
Bruce Markham <219281+brucificus@users.noreply.github.com>
2020-03-20 23:50:22 -0400

    👷 Adds Rudimentary Build Process

---
+++ index.ts
@@ -0,0 +1,4 @@
+export * from './antlr4ts_out/VisualBasic6Lexer';
+export * from './antlr4ts_out/VisualBasic6Parser';
+export * from './antlr4ts_out/VisualBasic6ParserListener';
+export * from './antlr4ts_out/VisualBasic6ParserVisitor';
